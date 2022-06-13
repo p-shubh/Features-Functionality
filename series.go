a55b0feaea7176f1ecddba1dfda2771d07451b6d
shubham prajapati <shubhamprajapati032@gmail.com>
2022-06-13 15:15:11 +0530

    series

---
+++ series.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Print the series : 1 1 2 3 5 8 13 ....")
+
+	series(7)
+
+	// count := 7
+
+}
+
+func series(a int) {
+
+	var j int = 1
+
+	var store int
+
+	var print int
+
+	for i := 0; i < a; i++ {
+		fmt.Println(store)
+		print = store + j // store = 1 + 1 = 2
+		if j == 1 {
+			j = store
+		} // j = 2
+
+		j = store
+		store = print
+
+	}
+
+}
