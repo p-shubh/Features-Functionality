e12d922916d296071f8dab6166c9bf3299d18337
shubham prajapati <shubhamprajapati032@gmail.com>
2022-06-13 17:59:50 +0530

    leap_year

--- leap_year.go
+++ leap_year.go
@@ -10,7 +10,7 @@ func main() {
 
 	fmt.Scan(&year)
 
-	if year%4 == 0 || year%400 == 0 {
+	if year%4 == 0 && year%400 == 0 || year%100 == 0 {
 		fmt.Println("Yes", year, "is a leap year")
 	} else {
 		fmt.Println(year, " is not a leap year")
